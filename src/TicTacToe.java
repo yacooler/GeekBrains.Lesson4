c86743a85d2ceb34a00bac58a0d9a634610a22de
yacooler <yacooler@yandex.ru>
2020-06-23 22:32:46 +0300

    Добавлен главный игровой цикл с проверкой на ничью и выигрыш, добавлен ход человека.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -11,30 +11,56 @@ public class TicTacToe {
 
         int FIELD_SIZE = 5;
         int LINE_SIZE = 4;
-        char CELLTYPE_EMPTY = '•';
-        char CELLTYPE_X = 'X';
-        char CELLTYPE_O = 'O';
+
+        char[] CELLTYPE= {'X','O','•'};
+        int CELLTYPE_EMPTY = 2;
 
         boolean[] isHumanPlayer = new boolean[2];
-        int currentPlayer = 1;
+
 
         Scanner scanner = new Scanner(System.in);
 
         selectPlayers(isHumanPlayer, scanner);
 
         char[][] field = new char[FIELD_SIZE][FIELD_SIZE];
-        clearField(field, CELLTYPE_EMPTY);
+        clearField(field, CELLTYPE[CELLTYPE_EMPTY]);
 
-//        char[][] field = {
-//                {'X','X','X','x','X'},
-//                {'.','X','x','X','X'},
-//                {'.','x','x','X','X'},
-//                {'.','x','X','X','X'},
-//                {'.','X','X','X','X'}};
+        gameLoop(field, isHumanPlayer, LINE_SIZE, CELLTYPE, scanner);
 
-        drawField(field);
 
-        System.out.println(checkWin(field,'.',4));
+    }
+
+    public static void gameLoop(char[][] field, boolean[] isHumanPlayer, int lineSize, char[] cellType, Scanner scanner ){
+        int CELLTYPE_EMPTY = 2;
+        int currentPlayer = 0;
+
+        while (true){
+
+            System.out.printf("Ход %d игрока!\n", (currentPlayer+1));
+            drawField(field);
+
+            if (checkDraw(field, cellType[CELLTYPE_EMPTY])){
+                System.out.println("Ничья!");
+                break;
+            }
+
+            if (isHumanPlayer[currentPlayer]){
+                humanMove(field, cellType[CELLTYPE_EMPTY], cellType[currentPlayer], scanner);
+            } else {
+                computerMove(field, cellType[CELLTYPE_EMPTY], cellType[currentPlayer]);
+            }
+
+            if (checkWin(field, cellType[currentPlayer], lineSize)){
+                System.out.printf("Игрок %d победил!\n", (currentPlayer+1));
+                drawField(field);
+                break;
+            }
+
+            currentPlayer = currentPlayer == 1 ? 0 : 1;
+
+        }
+
+
 
     }
 
@@ -42,16 +68,16 @@ public class TicTacToe {
     /**
      * Выбор типа игрока - человек true, компьютерный оппонент - false
      * @param isHumanPlayer массив, содержащий признак игрока - человека
-     * @param scanner ссылка на сканер для ввода данных
+     * @param scanner сканер для ввода данных
      */
     public static void selectPlayers(boolean[] isHumanPlayer, Scanner scanner){
         //Для большего количества можно было бы использовать цикл
         System.out.println("Введите тип первого и второго игрока");
-        System.out.println("Первый игрок - человек или ИИ? true - человек, false - ИИ");
-        isHumanPlayer[0] = scanner.nextBoolean();
+        System.out.println("Первый игрок - человек или ИИ? 1 - человек, 2 - ИИ");
+        isHumanPlayer[0] = 1 == scanner.nextInt();
 
-        System.out.println("Второй игрок - человек или ИИ? true - человек, false - ИИ");
-        isHumanPlayer[1] = scanner.nextBoolean();
+        System.out.println("Второй игрок - человек или ИИ? 1 - человек, 2 - ИИ");
+        isHumanPlayer[1] = 1 == scanner.nextInt();
 
         if (isHumanPlayer[0] && isHumanPlayer[1]){
             System.out.println("По очереди вводите координаты ячеек, в которые хотите сделать ход");
@@ -89,12 +115,12 @@ public class TicTacToe {
 
         System.out.print(" ");
         for (int i = 0; i < size; i++) {
-            System.out.print(" " + i + " ");
+            System.out.print(" " + (i+1) + " ");
         }
         System.out.println();
 
         for (int i = 0; i < size; i++) {
-            System.out.print(i);
+            System.out.print(i+1);
             for (int j = 0; j < size; j++) {
                 System.out.print(" " + field[i][j] + " ");
             }
@@ -129,61 +155,98 @@ public class TicTacToe {
      * @param lineSize размер непрерывной линии фишек, при достижении которой засчитывается победа
      * @return true если проверяемый игрок победил
      */
-
     public static boolean checkWin(char[][] field, char checkingChar, int lineSize){
         int size = field.length;
         int checkedIndex = size - lineSize;
-        boolean horizontalWin;
-        boolean verticalWin;
-        boolean principalDiagonalWin;
-        boolean secondaryDiagonalWin;
+        int loops = 0;
+        //Переменные указывают, может ли из точки y,x теоретически быть построена выигрышная комбинация
+        boolean horizontalWinPossible;
+        boolean verticalWinPossible;
+        boolean principalDiagonalWinPossible;
+        boolean secondaryDiagonalWinPossible;
 
-        /*Проверка на наличие полностью заполненных линий.
-        y,x содержат начальную координату для проверки
-        l - цикл по координатам от начальной точки проверки до длины линии
-        */
         for (int y = 0; y < size; y++) {
             for (int x = 0; x < size; x++) {
 
-                /*
-                    Инициализируем булевые переменные.
-                    если поиск для текущих X и Y валиден - ставим true.
-                    В цикле поиска если встретим запрещенный символ - ставим false
-                 */
-                horizontalWin = x <= checkedIndex;
-                verticalWin = y <= checkedIndex;
-                principalDiagonalWin = (y <= checkedIndex) && (x <= checkedIndex);
-                secondaryDiagonalWin = (y <= checkedIndex) && (x >= (lineSize - 1));
+                //Если поиск для текущих X и Y для выбранного направления валиден - ставим true
+                //Поиск идет вниз, вправо, влево вниз и вправо вниз от точки y,x
+                horizontalWinPossible = x <= checkedIndex;
+                verticalWinPossible = y <= checkedIndex;
+                principalDiagonalWinPossible = (y <= checkedIndex) && (x <= checkedIndex);
+                secondaryDiagonalWinPossible = (y <= checkedIndex) && (x >= (lineSize - 1));
 
-                for (int l = 0; l < lineSize; l++) {
-                    //Помним, что операции сравнения - ленивые. Можно проверять выход за границы массива и элемент массива разом
-                    //Проверка горизонтали. Перебираем все стартовые значения по Y, и только часть стартовых значений по X
-                    if ((x <= checkedIndex) && (checkingChar != field[y][x + l])) horizontalWin = false;
-                    //Проверка горизонтали. Перебираем часть стартовых значений по Y, и все значения по X
-                    if ((y <= checkedIndex) && (checkingChar != field[y + l][x])) verticalWin = false;
-                    //Проверка главной диагонали
-                    if ((y <= checkedIndex) && (x <= checkedIndex) && (checkingChar != field[y + l][x + l]))
-                        principalDiagonalWin = false;
-                    //Проверка побочной диагонали
-                    if ((y <= checkedIndex) && (x >= (lineSize - 1)) && (checkingChar != field[y + l][x - l]))
-                        secondaryDiagonalWin = false;
+                //l - цикл для проверки всех точек линии в 4х направлениях с начальными координатами y,x
+                for (int l = 0; l < lineSize && (horizontalWinPossible || verticalWinPossible || principalDiagonalWinPossible || secondaryDiagonalWinPossible); l++) {
+
+                    //Помним, что операции сравнения - ленивые. Можно проверять выход за границы массива и элемент массива в одном if
+                    if (horizontalWinPossible && (checkingChar != field[y][x + l])) horizontalWinPossible = false;
+                    if (verticalWinPossible   && (checkingChar != field[y + l][x])) verticalWinPossible = false;
+                    if (principalDiagonalWinPossible  && (checkingChar != field[y + l][x + l])) principalDiagonalWinPossible = false;
+                    if (secondaryDiagonalWinPossible  && (checkingChar != field[y + l][x - l])) secondaryDiagonalWinPossible = false;
+
+                    loops++;
                 }
-
-                System.out.println("Y:" + y + " X:" + x);
-
-                //Если нашли победу - выходим и дальше не сканируем
-                if (horizontalWin || verticalWin || principalDiagonalWin || secondaryDiagonalWin) {
-                    System.out.println("horizontalWin " + horizontalWin);
-                    System.out.println("verticalWin " + verticalWin);
-                    System.out.println("principalDiagonalWin " + principalDiagonalWin);
-                    System.out.println("secondaryDiagonalWin " + secondaryDiagonalWin);
-
-
+                //Победная комбинация символов в одном из направлений была достигнута
+                if (horizontalWinPossible || verticalWinPossible || principalDiagonalWinPossible || secondaryDiagonalWinPossible) {
+                    System.out.println(loops);
                     return true;
                 }
             }
         }
+        System.out.println(loops);
         return false;
     }
 
+    /**
+     * Ход игрока - человека. До хода нужно проверить состояние игрового поля на ничью
+     * @param field Матрица, содержащая игровое поле
+     * @param emptyCellChar Символ, означающий незанятую клетку
+     * @param playerChar Символ - фишка, принадлежащий игроку
+     */
+    public static void humanMove(char[][] field, char emptyCellChar, char playerChar, Scanner scanner){
+        int x;
+        int y;
+        int size = field.length;
+
+        while (true) {
+            System.out.println("Введите X координату");
+            x = humanInputCoordinate(field, scanner);
+
+            System.out.println("Введите Y координату");
+            y = humanInputCoordinate(field, scanner);
+
+            if (field[y-1][x-1] == emptyCellChar){
+                field[y-1][x-1] = playerChar;
+                return;
+            }
+
+            System.out.printf("Клетка для установки [%s] должна быть свободна\n", playerChar);
+            drawField(field);
+        }
+    }
+
+
+    /**
+     * Цикл запроса координат у человека
+     *
+     * @param field Матрица, содержащая игровое поле
+     * @param scanner сканер для ввода данных
+     * @return Координата, введенная пользователем
+     */
+    public static int humanInputCoordinate(char[][] field, Scanner scanner){
+        int coord;
+        while (true){
+            coord = scanner.nextInt();
+            if (coord < 1 || coord > field.length){
+                System.out.printf("Значение должно лежать в диапазоне от 1 до %d\n Введите значение:", field.length);
+            } else {
+                return coord;
+            }
+        }
+    }
+
+
+    public static void computerMove(char[][] field, char emptyCellChar, char playerChar) {
+        //TODO: do magic
+    }
 }
